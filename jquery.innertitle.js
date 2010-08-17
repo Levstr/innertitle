21c9121769f396795fd6ecc2c86dfc0203cd658b
Alex Glaskov <fotonstep@gmail.com>
2010-08-17 18:56:08 +0600

    [fix] margin

--- jquery.innertitle.js
+++ jquery.innertitle.js
@@ -1,5 +1,5 @@
 // Inner title JQuery plugin - shows titles inside input boxes automatically.
-// Requires JQuery 1.3
+// Requires JQuery 1.3+
 // (C) 2009, Nikolay Karev, karev.n@gmail.com
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 
@@ -26,25 +26,28 @@
         return;
       }
       $(this).addClass(options.inputClass);
-      var title = $(this).attr('title');
+
+      var title = '';
+      if(options.innerTitle != null) title = options.innerTitle;
+      else title = $(this).attr('title');
       if (title == '' || title == null){
         return;
       }
+
       var div = $('<div/>');
       var focused = false;
       var input = this;
-      div.html(title);  
+      div.html(title);
       $(div).css({
-        width: $(this).width(), 
-        height: $(this).height(),
         position: 'absolute',
-        top: $(this).position().top +  "px",
-        left: $(this).position().left + "px",
+        top: $(this).position().top + $(this).margin().top + "px",
+        left: $(this).position().left + $(this).margin().left + "px",
         zIndex: $(this).css('zindex') + 100
       });
       $(div).attr('class', $(this).attr('class'));
       $(div).addClass(options.overlayClass);
       $(this).offsetParent().append(div);
+
       if($(this).val() != ''){
         $(div).hide();
       }
@@ -56,26 +59,25 @@
         focused = false;
         if ($(this).val() == ''){
           $(div).show();
-        }    
+        }
       });
       $(this).focus(function(){
         focused = true;
         $(div).hide();
       });
       $(this).observe(function(){
-        if ($(input).val() == '' && !focused){
+        if ($(this).val() == '' && !focused){
           $(div).show();
         } else {
           $(div).hide();
         }
-
       },
       0.25);
     });
-
-
   }
+
+
   $.fn.innerTitle.enableGlobally = function(){
     $('body :input:visible').innerTitle();
   }
-  })(jQuery);
+})(jQuery);
